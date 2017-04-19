9aef9a7dcf11b6221fa4abe8c95070141a6b6969
zeggel <wickac@bk.ru>
2017-04-19 18:17:23 +0300

    Класс хелпер для преобразования xml строки в объект и обратно

---
+++ src/main/java/ru/platron/sdk/utils/XmlUtils.java
@@ -0,0 +1,48 @@
+package ru.platron.sdk.utils;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+
+import ru.platron.sdk.request.BaseRequest;
+
+public class XmlUtils {
+	public static Object fromXml(String xml, Class<?> cls) {
+		Object response = null;
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(cls);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			
+			response = jaxbUnmarshaller.unmarshal(new StreamSource(new StringReader(xml)));
+			
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return response;
+	}
+	
+	public static String toXml(BaseRequest request) {
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(request.getClass());
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+			
+			StringWriter writer = new StringWriter();
+			jaxbMarshaller.marshal(request, writer);
+			
+			return writer.toString();
+			
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "";
+	}
+}

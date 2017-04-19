2714e39c7d731aa5aab4efe30adfc3988406112c
zeggel <wickac@bk.ru>
2017-04-19 18:40:36 +0300

    Вспомогательный класс для чтения текста из файла

---
+++ src/test/java/ru/platron/sdk/Utils.java
@@ -0,0 +1,19 @@
+package ru.platron.sdk;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Utils {
+	public static String readFile(String path) {
+		String content = "";
+		
+		try {
+			content = new String(Files.readAllBytes(Paths.get(path)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return content;
+	}
+}

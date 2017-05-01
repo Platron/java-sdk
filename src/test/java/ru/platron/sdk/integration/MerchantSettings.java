c370f171f0bd2c1b3d033e9d56183dc1a0e3c311
zeggel <wickac@bk.ru>
2017-05-02 01:07:20 +0300

    Настройки магазина для интеграционного теста

---
+++ src/test/java/ru/platron/sdk/integration/MerchantSettings.java
@@ -0,0 +1,7 @@
+package ru.platron.sdk.integration;
+
+public class MerchantSettings {
+	public static String merchantId = "your_merchant_id";
+	
+	public static String secretKey = "your_secret_key";
+}

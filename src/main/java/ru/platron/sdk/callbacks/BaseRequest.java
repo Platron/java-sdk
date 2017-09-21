699d618e12d00489dfdcec972441a17e9885b1c4
zeggel <wickac@bk.ru>
2017-09-21 11:05:24 +0300

    Add cms_payment_module element to callback request base class

--- src/main/java/ru/platron/sdk/callbacks/BaseRequest.java
+++ src/main/java/ru/platron/sdk/callbacks/BaseRequest.java
@@ -3,6 +3,7 @@ package ru.platron.sdk.callbacks;
 import java.util.TreeMap;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
 
 import ru.platron.sdk.Signer;
 import ru.platron.sdk.XmlObject;
@@ -18,4 +19,7 @@ public class BaseRequest extends XmlObject {
 		
 		return signature.equals(this.signature);
 	}
+
+	@XmlElement(name = "cms_payment_module")
+	public String cmsPaymentModule = "platron-java-sdk";
 }
